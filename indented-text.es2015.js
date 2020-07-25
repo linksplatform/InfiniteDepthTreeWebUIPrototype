adc7ed94cff84acb36fbb9df44f66d07af68c2af
Konstantin Dyachenko <drakonard@gmail.com>
2020-07-25 15:39:12 +0300

    Indented text visualizer converted to ES2015 using babel.

--- indented-text.es2015.js
+++ indented-text.es2015.js
@@ -1,43 +1,54 @@
+"use strict";
+
 function indentedText(string) {
-    let lines = string.split('\n');
-    // Clear the surface
-    surface.innerHTML = '';
-    // Build a new tree
+    var lines = string.split('\n'); // Clear the surface
+
+    surface.innerHTML = ''; // Build a new tree
+
     var list = extractItems(lines);
-    surface.appendChild(list);
-    // Reset the surface
+    surface.appendChild(list); // Reset the surface
+
     moveToItem(getSurfaceFirstItem());
     refresh();
 }
 
 function extractItems(lines) {
-    let list = document.createElement("ul");
+    var list = document.createElement("ul");
+
     if (lines.length > 0) {
-        let firstLine = lines[0];
-        let firstLineIndentLength = firstLine.search(/\S/);
-        for (let i = 0; i < lines.length; i++) {
-            let itemIndentLength = lines[i].search(/\S/);
+        var firstLine = lines[0];
+        var firstLineIndentLength = firstLine.search(/\S/);
+
+        for (var i = 0; i < lines.length; i++) {
+            var itemIndentLength = lines[i].search(/\S/);
+
             if (itemIndentLength == firstLineIndentLength) {
-                let listItem = document.createElement("li");
-                let item = document.createElement("div");
+                var listItem = document.createElement("li");
+                var item = document.createElement("div");
                 item.classList.add("item");
                 item.innerText = lines[i];
                 listItem.appendChild(item);
-                let innerLines = [];
-                for (let j = i + 1; j < lines.length; j++) {
-                    let innerItemIndentLength = lines[j].search(/\S/);
+                var innerLines = [];
+
+                for (var j = i + 1; j < lines.length; j++) {
+                    var innerItemIndentLength = lines[j].search(/\S/);
+
                     if (innerItemIndentLength == itemIndentLength) {
                         break;
                     }
+
                     innerLines.push(lines[j].substring(2, lines[j].length));
                 }
+
                 if (innerLines.length > 0) {
-                    let innerList = extractItems(innerLines);
+                    var innerList = extractItems(innerLines);
                     listItem.appendChild(innerList);
                 }
+
                 list.appendChild(listItem);
             }
         }
     }
+
     return list;
 }
